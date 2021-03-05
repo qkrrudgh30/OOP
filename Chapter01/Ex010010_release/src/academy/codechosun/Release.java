a84485f993e7462698db5fb038f116c83b5b4fc7
CodeChosun <qkrrudgh30@gmail.com>
2021-03-05 23:27:32 +0900

    2021-03-05 OOP Chapter01. Java 기초 문법 1/2 완료.

---
+++ Chapter01/Ex010010_release/src/academy/codechosun/Release.java
@@ -0,0 +1,8 @@
+package academy.codechosun;
+
+public class Release {
+    public static void main(String[] args) {
+        System.out.printf("Let's get release this file!%s", System.lineSeparator());
+
+    } 
+}
